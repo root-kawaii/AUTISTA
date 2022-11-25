478017ef7cdb0d04bd38ba9a649e851b2e817b44
root-kawaii <teo.regge.99@gmail.com>
2022-11-25 22:41:26 +0000

    pollomarzo for the win with the react experience

--- client/src/App.js
+++ client/src/App.js
@@ -1,49 +1,76 @@
 // Importing modules
 import React, { useState, useEffect } from "react";
 import "./App.css";
+import AudioPlayer from "./AudioPlayer";
+import AudioRecorder from "./AudioRecorder";
 
 function App() {
-	// usestate for setting a javascript
-	// object for storing and using data
-	const [data, setdata] = useState({
-		name: "",
-		age: 0,
-		date: "",
-		keys : [""],
-		programming: "",
-	});
+  const [data, setData] = useState({
+    page: 0,
+  });
+  const [isLoading, setIsLoading] = useState(true);
+  const [err, setErr] = useState("");
 
-	// Using useEffect for single rendering
-	useEffect(() => {
-		// Using fetch to fetch the api from
-		// flask server it will be redirected to proxy
-		fetch("/data").then((res) =>
-			res.json().then((data) => {
-				// Setting a data from api
-				setdata({
-					name: data.Name,
-					age: data.Age,
-					date: data.Data,
-					programming: data.programming,
-					keys: data.Keys
-				});
-			})
-		);
-	}, []);
+  const fetchImages = () => {
+    setIsLoading(true);
+    fetch("/data?" + new URLSearchParams({ page: data.page }))
+      .then((res) => res.json())
+      .then(
+        (data) => {
+          setIsLoading(false);
+          setData({
+            ...data,
+            page: data.page + 1,
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components. thanks docs!
+        (error) => {
+          setIsLoading(false);
+          setErr(error);
+        }
+      );
+  };
 
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>COMMUTTI II GANG</h1>
-				<h3>We stand for legal weed</h3>
-				{/* Calling a data from setdata for showing */}
-				<p>{data.name}</p>
-				<p>{data.age}</p>
-				<p>{data.date}</p>
-			<img src={"https://aui20222.s3.eu-central-1.amazonaws.com/"+ data.keys[0]} alt="br" heigt={300} width={300}></img>
-			</header>	
-		</div>
-	);
+  // load initial state
+  useEffect(fetchImages, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        {err ? (
+          <h2>{err.message}</h2>
+        ) : isLoading ? (
+          <h2>loading... </h2>
+        ) : (
+          <>
+            <p>name: {data.name}</p>
+            <p>age: {data.age}</p>
+            {/* <img
+              src={
+                "https://aui20222.s3.eu-central-1.amazonaws.com/" + data.keys[0]
+              }
+              alt="br"
+              heigt={300}
+              width={300}
+            ></img> */}
+            {data.test_images.map((img, i) => (
+              <div key={`img_${i}`}>{img}</div>
+            ))}
+            {/* update every click... */}
+            <button onClick={fetchImages}>next page!</button>
+            {/* <AudioPlayer
+              url={
+                "https://aui20222.s3.eu-central-1.amazonaws.com/audioBanana.aac"
+              }
+            />
+            <AudioRecorder /> */}
+          </>
+        )}
+      </header>
+    </div>
+  );
 }
 
 export default App;
--- flask-server/server.py
+++ flask-server/server.py
@@ -1,60 +1,34 @@
-import boto3
-import pandas as pd
-from flask import Flask,request
+import datetime
+from flask import Flask, request
 import json
 
-# Make dataframes
-foo = pd.DataFrame({'x': [1, 2, 3], 'y': ['a', 'b', 'c']})
-bar = pd.DataFrame({'x': [10, 20, 30], 'y': ['aa', 'bb', 'cc']})
 
-# Save to csv
-foo.to_csv('foo.csv')
-bar.to_csv('bar.csv')
-
-with open('auth.txt') as file:
-    Lines = [line.rstrip() for line in file]
-
-s3 = boto3.resource(
-    service_name='s3',
-    region_name='eu-central-1',
-    aws_access_key_id=Lines[0],
-    aws_secret_access_key=Lines[1]
-)
-
-# Upload files to S3 bucket
-s3.Bucket('aui20222').upload_file(Filename='foo.csv', Key='foo.csv')
-s3.Bucket('aui20222').upload_file(Filename='bar.csv', Key='bar.csv')
-
-obj_list_key = []
-for obj in s3.Bucket('aui20222').objects.all():
-    print(type(obj.key))
-    obj_list_key.append(obj.key)
-    
-    
 # Import flask and datetime module for showing date and time
-from flask import Flask
-import datetime
 
 x = datetime.datetime.now()
 
 # Initializing flask app
 app = Flask(__name__)
 
-
+# http://127.0.0.1:5000/data
 # Route for seeing a data
+
+
 @app.route('/data')
 def get_time():
+    # Returning an api for showing in reactjs
+    page = int(request.args.get('page'))
+    print("current page request: " + str(page))
+    return {
+        'name': "geek",
+        "age": "22",
+        # "keys": obj_list_key,
+        "test_images": [page + 1, page + 2, page + 3],
+        "data": x,
+        "page": page,
+    }
 
-	# Returning an api for showing in reactjs
-	return {
-		'Name':"geek",
-		"Age":"22",
-		"Keys": obj_list_key,
-        "Data": x,
-		"programming":"python"
-		}
 
-	
 # Running app
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
