ea79857b07967304d687db7cc89482a02236c1d7
FraLeone837 <10572353@polimi.it>
2023-02-03 01:09:34 +0100

    New animation tested

--- client/src/pages/testMotion.js
+++ client/src/pages/testMotion.js
@@ -6,18 +6,54 @@ import protagonista from "../Assets/UI/protagonista.png";
 // <motion.div animate={{ x: 100 }} />
 
 function TestMotion() {
+
   return (
       <div className="vabene">
-        <motion.img src={protagonista}
-                    animate={{ x: [0, 0, 200], y:[0, 200, 0]}}
-                    transition={{ duration: 2 , delay:1}}
 
 
+          <motion.button
+                  style={{
+                      width: 400,
+                      height: 400,
+                      // backgroundColor: "#fff",
+                      background:"none",
+                      border: "none"
+                   }}
 
-        />
+                animate={{ x: [0, 100, 300], y:[0, -50, 20], scale: [0.9, 0.5, 1.2]}}
+                transition={{repeat:0, duration: 4 , delay:1, times: [0, 0.4, 1], }}
+          >
+
+                <motion.img src={protagonista}
+                    whileHover={{
+                        scale: 1.4,
+                        transition: { duration: 0.5 }
+                    }}
+                    whileTap={{ scale: 0.9 }}
+                />
+        </motion.button>
       </div>
 
   );
+  
+
+        //drag and move
+                    // drag
+                    // dragConstraints={{
+                    //   top: -50,
+                    //   left: -50,
+                    //   right: 50,
+                    //   bottom: 50,
+                    // }}
+
+
+        //appear
+                    // initial={{ opacity: 0 }}
+                    // whileInView={{ opacity: 1 }}
+                    // viewport={{ once: true }}
+                    // transition={{ duration: 2 , delay:1}}
 }
+
+
 
 export default TestMotion;
